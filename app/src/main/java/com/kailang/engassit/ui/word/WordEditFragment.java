ec7b2b477ad0769c4a4aba770760313867d252e6
kailanguage <41792700+kailanguage@users.noreply.github.com>
2020-05-11 23:12:59 +0800

    update WordEditFragment

--- app/src/main/java/com/kailang/engassit/ui/word/WordEditFragment.java
+++ app/src/main/java/com/kailang/engassit/ui/word/WordEditFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.google.gson.Gson;
 import com.kailang.engassit.R;
@@ -65,6 +66,7 @@ public class WordEditFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 wordViewModel.deleteWord(word);
+                Navigation.findNavController(v).navigateUp();
             }
         });
 
